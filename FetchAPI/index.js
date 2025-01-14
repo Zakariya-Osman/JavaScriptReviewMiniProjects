bf25c70f9ae30e2fb0d88676727ed43737e9b35b
Zackaria Osman <cobalt.zr86@gmail.com>
2025-01-14 14:12:07 -0700

    Working on render

--- FetchAPI/index.js
+++ FetchAPI/index.js
@@ -1,23 +1,85 @@
-// fetch("https://pokeapi.co/api/v2/pokemon/dittao")
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
 //   .then((response) => {
 //     if (!response.ok) {
 //       throw new Error("HTTP error, status = " + response.status);
 //     }
 //     return response.json();
 //   })
-//   .then((data) => console.log(data.name))
+//   .then((data) => console.log(data.))
 //   .catch((error) => console.error("Error: ", error));
 
-function getPokemonData(pokemonName, typeOfData) {
-  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+list = [];
+
+function getPokemonData() {
+  let userInput = document.getElementById("pokemon").value.trim().toLowerCase();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${userInput}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error("HTTP error, status = " + response.status);
       }
       return response.json();
     })
-    .then((data) => console.log(data[typeOfData]))
+    .then((data) => {
+      let pokemonInfo = {
+        name: data.name,
+        // weight: data.weight,
+        // height: data.height,
+        // abilities: data.abilities,
+        // types: data.types,
+        sprites: data.sprites.front_default, // Correct the typo
+      };
+      console.log(pokemonInfo);
+      return pokemonInfo;
+    })
     .catch((error) => console.error("Error: ", error));
 }
 
-getPokemonData("ditto", "weight");
+// function echoPokemonName() {
+//   let bee = document.getElementById("pokemon").value;
+//   console.log(bee);
+// }
+
+//getPokemonData("beedrill", "name");
+
+// function optionSelectedAttribute(list) {
+//   for (let i = 0; i < list.length; i++) {
+//     let option = document.createElement("option");
+//     option.textContent = list[i];
+//     document.getElementById("pokemon").appendChild(option);
+//   }
+// }
+
+// function getAllTypesOfData(pokemonName) {
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("HTTP error, status = " + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(Object.keys(data)); // Use bracket notation to access dynamic property
+//     })
+//     .catch((error) => console.error("Error: ", error));
+// }
+
+// function getAllTypesOfData(pokemonName) {
+//   var list = [];
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("HTTP error, status = " + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => {
+//       list.push(Object.keys(data));
+//       return list[1];
+//     })
+//     .catch((error) => console.error("Error: ", error));
+// }
+
+// console.log(getAllTypesOfData("beedrill"));
+
+//getPokemonData("ditto", "weight");
+//function getAllTypesOfData()
